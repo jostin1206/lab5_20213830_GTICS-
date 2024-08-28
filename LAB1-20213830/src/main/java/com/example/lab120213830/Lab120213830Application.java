c16c82a073974887b35782ff14eebc0a9d3f01e4
Jostin <a20213830@pucp.edu.pe>
2024-08-28 18:13:31 -0500

    configuraciones

---
+++ LAB1-20213830/src/main/java/com/example/lab120213830/Lab120213830Application.java
@@ -0,0 +1,14 @@
+package com.example.lab120213830;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120213830Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab120213830Application.class, args);
+    }
+
+
+}
---
+++ LAB1-20213830/src/test/java/com/example/lab120213830/Lab120213830ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab120213830;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120213830ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
