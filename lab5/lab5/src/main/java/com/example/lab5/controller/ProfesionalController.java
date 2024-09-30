53075e7d7319b4c7475d653a830c27e904a0a0cc
jostin1206 <a20213830@pucp.edu.pe>
2024-09-29 22:38:08 -0500

    agregué unas lineas en properties si en caso no se corre el proyecto

--- lab5/lab5/src/main/java/com/example/lab5/controller/ProfesionalController.java
+++ lab5/lab5/src/main/java/com/example/lab5/controller/ProfesionalController.java
@@ -1,18 +1,30 @@
 package com.example.lab5.controller;
 
+import com.example.lab5.entity.Profesionales;
+import com.example.lab5.repository.AreasRepository;
 import com.example.lab5.repository.ProfesionalRepository;
+import com.example.lab5.repository.SedesRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ProfesionalController {
 
     final ProfesionalRepository profesionalRepository;
+    private final AreasRepository areasRepository;
+    private final SedesRepository sedesRepository;
 
-    public ProfesionalController(ProfesionalRepository profesionalRepository) {
+    public ProfesionalController(ProfesionalRepository profesionalRepository,
+                                 AreasRepository areasRepository,
+                                 SedesRepository sedesRepository) {
 
         this.profesionalRepository = profesionalRepository;
+        this.areasRepository = areasRepository;
+        this.sedesRepository = sedesRepository;
     }
 
 
--- lab5/lab5/src/main/java/com/example/lab5/repository/ProfesionalRepository.java
+++ lab5/lab5/src/main/java/com/example/lab5/repository/ProfesionalRepository.java
@@ -2,8 +2,14 @@ package com.example.lab5.repository;
 
 import com.example.lab5.entity.Profesionales;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProfesionalRepository extends JpaRepository<Profesionales, Integer> {
+
+
 }
